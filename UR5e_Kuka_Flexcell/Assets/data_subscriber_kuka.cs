644d7cbaa4231b6244bf8d7d7f78232c6eef3d7e
Santiago Gil Arboleda <au698550@uni.au.dk>
2023-03-29 11:38:27 +0200

    adding unity app

---
+++ UR5e_Kuka_Flexcell/Assets/data_subscriber_kuka.cs
@@ -0,0 +1,148 @@
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
+using System;
+using System.Globalization;
+using UnityEngine;
+using NetMQ;
+using NetMQ.Sockets;
+using Unity.Robotics.UrdfImporter.Control;
+//using KukaController.Control;
+public class data_subscriber_kuka : MonoBehaviour
+{
+    string[] topics = {"actual_q_0","actual_q_1", "actual_q_2", "actual_q_3", "actual_q_4", "actual_q_5", "actual_q_6"};
+    private Thread _listenerWorker;
+    bool done = false;
+    Controller _robotarm_ctrl;
+
+     public delegate void MessageDelegate(string message);
+
+    private readonly MessageDelegate _messageDelegate;
+
+    private readonly ConcurrentQueue<string[]> _messageQueue = new ConcurrentQueue<string[]>();
+    public bool enable_tcp_connection = true;
+    public bool firstTime = true;
+    
+
+
+    // Start is called before the first frame update
+
+    void Listener(){
+        try
+        {
+            using (var subSocket = new SubscriberSocket()){
+                //Debug.Log("Starting subscriber for Topics: " + topics[0]);
+                subSocket.Connect("tcp://localhost:5557");
+                //subSocket.Connect("tcp://192.168.1.151:5557");
+                Debug.Log("Kuka subscriber socket connecting...");
+                for (int i = 0; i < topics.GetLength(0); ++i)
+                    subSocket.Subscribe(topics[i]);
+
+                while (!done)
+                {
+                    string[] bytes = Encoding.Default.GetString(subSocket.ReceiveFrameBytes()).Split(' ');
+                    //Debug.Log(bytes[0] + " " + bytes[1]);
+                    _messageQueue.Enqueue(bytes);
+                }
+                subSocket.Close();
+            }
+            
+            NetMQConfig.Cleanup();
+        }
+        catch (SocketException e)
+        {
+            Debug.Log("SocketException occured source: " + e.Message);
+        }
+    }
+
+    void HandleMessage(string[] message)
+    {
+        // Debug.Log("Handling message");
+        char topic_index = message[0][message[0].Length - 1];
+        ArticulationBody[] articulationChain = _robotarm_ctrl.GetComponentsInChildren<ArticulationBody>();
+        int joint_index = (int)Char.GetNumericValue(topic_index) + 1;
+        float theta_val_msg = float.Parse(message[1],CultureInfo.InvariantCulture.NumberFormat);
+        //Debug.Log("Joint value (after parsing): " + theta_val_msg);
+        float theta_val_div = (float) (theta_val_msg / Math.PI);
+        float theta_val = (float)(theta_val_div * 180.0);
+        //float theta_val = (float)(float.Parse(message[1]) * 180/Math.PI);
+        //Debug.Log("topic: " + topic_index + ", theta: " + theta_val);
+        //_robotarm_ctrl.UpdateJointTheta(joint_index, theta_val);
+        UpdateJointTheta(joint_index, theta_val);
+        //if (joint_index == 6)
+        //    Debug.Log("joint 6, theta: " + theta_val);
+        var twist = articulationChain[joint_index].jointPosition[0] * 180/Math.PI;
+        //Debug.Log("Twist: ");
+        //Debug.Log(twist);
+    }
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+        _robotarm_ctrl = gameObject.GetComponent<Controller>();
+        
+
+        if (enable_tcp_connection)
+        {
+            
+
+            done = false;
+            _listenerWorker = new Thread(Listener);
+            _listenerWorker.Start();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstTime == true){
+            UpdateJointTheta(1, 0.0f);
+            UpdateJointTheta(2, 0.0f);
+            UpdateJointTheta(3, 0.0f);
+            UpdateJointTheta(4, 0.0f);
+            UpdateJointTheta(5, 0.0f);
+            UpdateJointTheta(6, 0.0f);
+            UpdateJointTheta(7, 0.0f);
+            firstTime = false;
+        }
+        while (!_messageQueue.IsEmpty)
+        {
+            string[] message;
+            if (_messageQueue.TryDequeue(out message))
+            {
+                HandleMessage(message);
+            }
+            else
+            {
+                break;
+            }
+        }
+            
+    }
+
+    private void OnDestroy()
+    {
+        done = true;
+        if (enable_tcp_connection)
+        {
+            _listenerWorker.Join();
+        }
+    }
+
+    void UpdateJointTheta(int jointIndex, float target_theta)
+        {
+            //Debug.Log(target_theta);
+            ArticulationBody[] robotArticulationChain = _robotarm_ctrl.GetComponentsInChildren<ArticulationBody>();
+            JointControl joint = robotArticulationChain[jointIndex].GetComponent<JointControl>();
+            joint.controltype = _robotarm_ctrl.control;
+            if (_robotarm_ctrl.control == ControlType.PositionControl)
+            {
+                ArticulationDrive drive = joint.joint.xDrive;
+                drive.stiffness = _robotarm_ctrl.stiffness;
+                drive.damping = _robotarm_ctrl.damping;
+                drive.target = target_theta;
+                joint.joint.xDrive = drive;
+            }
+    }
+}
